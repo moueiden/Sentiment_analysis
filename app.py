07e2918781584144573e19fdaa1a8ea0ff5e2998
Oueideng-Modeste <modeste.oueideng@dhl.com>
2023-12-07 10:38:11 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,141 @@
+
+import streamlit as st  
+from textblob import TextBlob
+import pandas as pd
+import altair as alt
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk import PorterStemmer
+PorterStemmer().stem('complications')
+
+# Fxn
+def convert_to_df(sentiment):
+	sentiment_dict = {'polarity':sentiment.polarity,'subjectivity':sentiment.subjectivity}
+	sentiment_df = pd.DataFrame(sentiment_dict.items(),columns=['metric','value'])
+	return sentiment_df
+
+def analyze_token_sentiment(docx):
+	analyzer = SentimentIntensityAnalyzer()
+	pos_list = []
+	neg_list = []
+	neu_list = []
+	for i in docx.split():
+		res = analyzer.polarity_scores(i)['compound']
+		if res > 0.1:
+			pos_list.append(i)
+			pos_list.append(res)
+
+		elif res <= -0.1:
+			neg_list.append(i)
+			neg_list.append(res)
+		else:
+			neu_list.append(i)
+
+	result = {'positives':pos_list,'negatives':neg_list,'neutral':neu_list}
+	return result 
+
+
+
+
+		
+
+
+def main():
+	st.title("Aritificial Intelligence")
+	st.subheader("NLP Projects")
+
+	menu = ["English","French"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	if choice == "English":
+		st.subheader("Welcome to Sentiment Analysis!")
+		with st.form(key='nlpForm'):
+			raw_text = st.text_area("Please, Enter Your Text Here")
+			submit_button = st.form_submit_button(label='Analyze')
+
+		# layout
+		col1,col2 = st.columns(2)
+		if submit_button:
+
+			with col1:
+				st.info("Results")
+				sentiment = TextBlob(raw_text).sentiment
+				st.write(sentiment)
+
+				# Emoji
+				if sentiment.polarity > 0:
+					st.markdown("Sentiment:: Positive :smiley: ")
+				elif sentiment.polarity < 0:
+					st.markdown("Sentiment:: Negative :angry: ")
+				else:
+					st.markdown("Sentiment:: Neutral 😐 ")
+
+				# Dataframe
+				result_df = convert_to_df(sentiment)
+				st.dataframe(result_df)
+
+				# Visualization
+				c = alt.Chart(result_df).mark_bar().encode(
+					x='metric',
+					y='value',
+					color='metric')
+				st.altair_chart(c,use_container_width=True)
+
+
+
+			with col2:
+				st.info("Token Sentiment")
+
+				token_sentiments = analyze_token_sentiment(raw_text)
+				st.write(token_sentiments)
+
+
+
+
+
+
+	else:
+		st.subheader("French")
+	if choice == "French":
+		st.subheader("Soyez la Bienvenue!!")
+		with st.form(key='nlpForm'):
+			raw_text = st.text_area("Veuillez Saisir votre Text")
+			submit_button = st.form_submit_button(label='Analyse')
+
+		# layout
+		col1, col2 = st.columns(2)
+		if submit_button:
+
+			with col1:
+				st.info("Results")
+				sentiment = TextBlob(raw_text).sentiment
+				st.write(sentiment)
+
+				# Emoji
+				if sentiment.polarity > 0:
+					st.markdown("Sentiment:: Positive :smiley: ")
+				elif sentiment.polarity < 0:
+					st.markdown("Sentiment:: Negative :angry: ")
+				else:
+					st.markdown("Sentiment:: Neutral 😐 ")
+
+				# Dataframe
+				result_df = convert_to_df(sentiment)
+				st.dataframe(result_df)
+
+				# Visualization
+				c = alt.Chart(result_df).mark_bar().encode(
+					x='metric',
+					y='value',
+					color='metric')
+				st.altair_chart(c, use_container_width=True)
+
+			with col2:
+				st.info("Token Sentiment")
+
+				token_sentiments = analyze_token_sentiment(raw_text)
+				st.write(token_sentiments)
+
+
+if __name__ == '__main__':
+	main()
+
